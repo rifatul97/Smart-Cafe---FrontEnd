11577cba9e0f1dcfde701147ef76d4625f60908d
rifatul97 <rifatulkarim1997@gmail.com>
2021-11-25 11:50:25 -0500

    1 file modified

--- components/Menu.js
+++ components/Menu.js
@@ -8,6 +8,13 @@ import Modal from '@mui/material/Modal';
 import Fade from '@mui/material/Fade';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
@@ -199,14 +206,62 @@ export default function Menu(props) {
       );
     } else {
       console.log(products);
+      
+      const rows = [];
+
+      
+      let ans = '';
       for (var cart of userCart) {
-        //products[1].name;
+        for (var product of products) {
+          if (cart.productId == product.id) {
+            let name = JSON.stringify(product.name);
+            let qt = JSON.stringify(cart.quantity);
+            row.push(createData(name, qt, (product.price * cart.quantity))))
+          }
+        }
       }
-      for (var product of products) {
-        //console.log(product);
-      }
-      return <div></div>;
+
+      return <div>{ans}</div>;
     }
+  }
+
+  export default function BasicTable() {
+    return (
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Dessert (100g serving)</TableCell>
+              <TableCell align="right">Calories</TableCell>
+              <TableCell align="right">Fat&nbsp;(g)</TableCell>
+              <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+              <TableCell align="right">Protein&nbsp;(g)</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows.map((row) => (
+              <TableRow
+                key={row.name}
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  {row.name}
+                </TableCell>
+                <TableCell align="right">{row.calories}</TableCell>
+                <TableCell align="right">{row.fat}</TableCell>
+                <TableCell align="right">{row.carbs}</TableCell>
+                <TableCell align="right">{row.protein}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    );
+  }
+  
+
+  function createData(name, quantity, totalPrice) {
+    return { name, quantity, totalPrice };
   }
 
   const handleProductClickAction = (val) => {
